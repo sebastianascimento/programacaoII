6f109b15db21f3a1360235339771f857d86f5eb5
Sebastião <sebastiao.alpes@hotmail.com>
2024-01-17 19:21:27 +0000

    aula2

---
+++ aula2/app.js
@@ -0,0 +1,20 @@
+const menua= (x) => {
+
+    let num = 20
+
+    return num + int;
+}
+
+let int = 10;
+
+console.log(menua(int));
+
+
+
+
+
+const addnumero = (a ,b) => {
+    return a + b ;
+} 
+
+console.log(addnumero( 56 , 7));
